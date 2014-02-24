b463b673a9f09e0b037ac907461887db69b18126
Mick Tarsel <mtarsel@gmail.com>
2014-02-24 16:36:18 -0500

    Displays cube with shaders on both ends. Code needs cleaning up

--- lab4.cpp
+++ lab4.cpp
@@ -1,215 +1,244 @@
-#include "shaders.h"
+#include "initShaders.h"
 #include <cstdlib>
 using namespace std;
 
 void rotate(GLuint locate);
 
-GLuint vaoID,vboID[2],eboID;
+GLuint vaoID,vboID[3],eboID;
 GLuint program;
 
 GLfloat pit,yaw,scalar=1;
 glm::vec3 cubeTran;
 
 GLfloat size=10;
-
+GLfloat ncomp = 1.0f / sqrt(3.0f); // for normal vectors
 
 GLfloat vertexarray[]={size,size,-size,
-                                   size,-size,-size,
+                       size,-size,-size,
                        -size,-size,-size,
                        -size,size,-size,
                        size,size,size,
                        size,-size,size,
                        -size,-size,size,
                        -size,size,size
-                       };
+};
 
-
+GLfloat normsarray[] = {ncomp,ncomp,-ncomp,
+                       ncomp,-ncomp,-ncomp,
+                       -ncomp,-ncomp,-ncomp,
+                       -ncomp,ncomp,-ncomp,
+                       ncomp,ncomp,ncomp,
+                       ncomp,-ncomp,ncomp,
+                       -ncomp,-ncomp,ncomp,
+                       -ncomp,ncomp,ncomp
+};
 
 GLfloat colorarray[]={
-			    0.5f,1.0f,1.0f,1.0f,
-				    1.0f,0.5f,1.0f,1.0f,
-				    1.0f,1.0f,0.5f,1.0f,
-				    1.0f,1.0f,1.0f,1.0f,
-				    0.5f,1.0f,1.0f,1.0f,
-				    1.0f,0.5f,1.0f,1.0f,
+/*
+		    1.0f,0,0,1.0f,
+                      1.0f,0,0,1.0f,
+                      1.0f,0,0,1.0f,
+                      1.0f,0,0,1.0f,
+                      1.0f,0,0,1.0f,
+                      1.0f,0,0,1.0f,
+                      1.0f,0,0,1.0f,
+                      1.0f,0,0,1.0f*/
+                            0.5f,1.0f,1.0f,1.0f,
+                                    1.0f,0.5f,1.0f,1.0f,
+                                    1.0f,1.0f,0.5f,1.0f,
+                                    1.0f,1.0f,1.0f,1.0f,
+                                    0.5f,1.0f,1.0f,1.0f,
+                                    1.0f,0.5f,1.0f,1.0f,
                       1.0f,1.0f,0.5f,1.0f
 
 
 
 };
 
-											
-GLubyte elems[]={0,1,2,3,7,4,5,6,
-    	          7,3,0,4,5,6,2,1,
-    		  0,1,5,4,7,3,2,6};
+GLubyte elems[] = {0,1,2,
+                   3,0,
+                   4,7,
+                   3,0,
+                   1,5,
+                   4,0,
+                   3,7,
+                   6,2,
+                   3,2,
+                   5,6,2,1,
+                   5,6,2,1,5,4,7,6,
+                   4,5,6,7,4};
 
 
 void init(){
-    glEnable(GL_DEPTH_TEST);
-    glViewport(0, 0, 600, 600);
-	
-    glGenVertexArrays(1,&vaoID);
-    glBindVertexArray(vaoID);
-	
-    glGenBuffers(2, vboID);
-    glBindBuffer(GL_ARRAY_BUFFER,vboID[0]);
-    glBufferData(GL_ARRAY_BUFFER,sizeof(vertexarray),vertexarray,GL_STATIC_DRAW);
-    glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,0,(void*)0);
-	
-    glBindBuffer(GL_ARRAY_BUFFER, vboID[1]);
-    glBufferData(GL_ARRAY_BUFFER,sizeof(colorarray),colorarray,GL_STATIC_DRAW);
-    glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 0, (void*)0);
+  glEnable(GL_DEPTH_TEST);
+  glEnable(GL_COLOR_MATERIAL);
+  glEnable(GL_LIGHTING);
+  glEnable(GL_LIGHT0);
+  glEnable(GL_LIGHT1);
+  glEnable(GL_NORMALIZE);
   
-    glGenBuffers(1,&eboID);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,eboID);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER,sizeof(elems),elems,GL_STATIC_DRAW);
-
-    ShaderInfo shaders[]={
-	{ GL_VERTEX_SHADER , "vertexshader.glsl"},
-	{ GL_FRAGMENT_SHADER , "fragmentshader.glsl"}, 
-	{ GL_NONE , NULL} 
-    };
+  glViewport(0, 0, 600, 600);
 	
-/*light shit from example.c*/
-   glEnable(GL_LIGHTING);
-   glEnable(GL_LIGHT0);
-   glEnable(GL_LIGHT1);
-   glEnable(GL_DEPTH_TEST);
-
-
-  //Add positioned light
-        GLfloat lightColor0[] = {2.5f, 2.5f, 2.5f, 2.0f}; //Color (0.5, 0.5, 0.5)
-        GLfloat lightPos0[] = {4.0f, 0.0f, 8.0f, 1.0f}; //Positioned at (4, 0, 8)
-        glLightfv(GL_LIGHT0, GL_DIFFUSE, lightColor0);
-        glLightfv(GL_LIGHT0, GL_POSITION, lightPos0);
-
-
-     //Right
-        glNormal3f(1.0f, 0.0f, 0.0f);
-        //glNormal3f(1.0f, 0.0f, -1.0f);
-        glVertex3f(1.5f, -1.0f, -1.5f);
-        //glNormal3f(1.0f, 0.0f, -1.0f);
-        glVertex3f(1.5f, 1.0f, -1.5f);
-        //glNormal3f(1.0f, 0.0f, 1.0f);
-        glVertex3f(1.5f, 1.0f, 1.5f);
-        //glNormal3f(1.0f, 0.0f, 1.0f);
-        glVertex3f(1.5f, -1.0f, 1.5f);
-
-
-
-      //Left
-        glNormal3f(-1.0f, 0.0f, 0.0f);
-        //glNormal3f(-1.0f, 0.0f, -1.0f);
-        glVertex3f(-1.5f, -1.0f, -1.5f);
-        //glNormal3f(-1.0f, 0.0f, 1.0f);
-        glVertex3f(-1.5f, -1.0f, 1.5f);
-        //glNormal3f(-1.0f, 0.0f, 1.0f);
-        glVertex3f(-1.5f, 1.0f, 1.5f);
-        //glNormal3f(-1.0f, 0.0f, -1.0f);
-        glVertex3f(-1.5f, 1.0f, -1.5f);
-
-
-
+  glGenVertexArrays(1,&vaoID);
+  glBindVertexArray(vaoID);
 	
-    program=initShaders(shaders);
+  glGenBuffers(3, vboID);
+  glBindBuffer(GL_ARRAY_BUFFER,vboID[0]);
+  glBufferData(GL_ARRAY_BUFFER,sizeof(vertexarray),vertexarray,GL_STATIC_DRAW);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+	
+  glBindBuffer(GL_ARRAY_BUFFER, vboID[1]);
+  glBufferData(GL_ARRAY_BUFFER,sizeof(colorarray),colorarray,GL_STATIC_DRAW);
+  glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 0, (void*)0);
+
+  glBindBuffer(GL_ARRAY_BUFFER, vboID[2]);
+  glBufferData(GL_ARRAY_BUFFER,sizeof(normsarray),normsarray,GL_STATIC_DRAW);
+  glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+
   
-    glEnableVertexAttribArray(0);
-    glEnableVertexAttribArray(1);
+  glGenBuffers(1,&eboID);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,eboID);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER,sizeof(elems),elems,GL_STATIC_DRAW);
+
+  ShaderInfo shaders[]={
+    { GL_VERTEX_SHADER , "vertexshader.glsl"},
+    { GL_FRAGMENT_SHADER , "fragmentshader.glsl"}, 
+    { GL_NONE , NULL} 
+  };
+		
+  program=initShaders(shaders);
+  glEnableVertexAttribArray(0);
+  glEnableVertexAttribArray(1);
+  glEnableVertexAttribArray(2);
   
 }
 
 
 void display(SDL_Window* screen){
-    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+  glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
 	
-    glm::mat4 trans;
-	
-    trans=glm::translate(trans,cubeTran);//translate the cube
-    trans=glm::rotate(trans,pit,glm::vec3(1,0,0));//rotate the cube around the x axis
-    trans=glm::rotate(trans,yaw,glm::vec3(0,1,0));//rotate the cube arround the y axis
-    trans=glm::scale(trans,glm::vec3(scalar));//scaling the cube
-    
-    GLint tempLoc = glGetUniformLocation(program,"modelMatrix");//Matrix that handle the transformations
-    glUniformMatrix4fv(tempLoc,1,GL_FALSE,&trans[0][0]);
-	
-    glDrawElements(GL_POLYGON,24,GL_UNSIGNED_BYTE,NULL);
-    glFlush();
-    SDL_GL_SwapWindow(screen);
+  glm::mat4 trans;
+
+  trans=glm::translate(trans,cubeTran);//translate the cube
+  trans=glm::rotate(trans,pit,glm::vec3(1,0,0));//rotate the cube around the x axis
+  trans=glm::rotate(trans,yaw,glm::vec3(0,1,0));//rotate the cube arround the y axis
+  trans=glm::scale(trans,glm::vec3(scalar));//scaling the cube
+
+  GLfloat ambient[] = {0.5f, 0.5f, 0.5f, 1.0f};
+  GLfloat light1_dir[] = {20.0f, 20.0f, 20.0f};
+  GLfloat light1_color[] = {0.1f, 1.0f, 0.1f};
+  GLfloat light2_dir[] = {-20.0f, -20.0f, -20.0f};
+  GLfloat light2_color[] = {0.1f, 0.1f, 1.0f};
+  GLint tempLoc = glGetUniformLocation(program,"modelMatrix");//Matrix that handle the transformations
+  glUniformMatrix4fv(tempLoc,1,GL_FALSE,&trans[0][0]);
+
+  tempLoc = glGetUniformLocation(program,"Ambient");
+  glUniform4fv(tempLoc,1,ambient);
+
+  tempLoc = glGetUniformLocation(program,"LightColor1");
+  glUniform3fv(tempLoc,1,light1_color);
+
+  tempLoc = glGetUniformLocation(program,"LightDirection1");
+  glUniform3fv(tempLoc,1,light1_dir);
+  
+  tempLoc = glGetUniformLocation(program,"HalfVector1");
+  glUniform3fv(tempLoc,1,light1_dir);
+
+  tempLoc = glGetUniformLocation(program,"LightColor2");
+  glUniform3fv(tempLoc,1,light2_color);
+
+  tempLoc = glGetUniformLocation(program,"LightDirection2");
+  glUniform3fv(tempLoc,1,light2_dir);
+  
+  tempLoc = glGetUniformLocation(program,"HalfVector2");
+  glUniform3fv(tempLoc,1,light2_dir);
+
+  
+  glDrawElements(GL_TRIANGLE_STRIP,36,GL_UNSIGNED_BYTE,NULL);
+  glFlush();
+  SDL_GL_SwapWindow(screen);
 }
 
 void input(SDL_Window* screen){
 
-    SDL_Event event;
+  SDL_Event event;
 
-    while (SDL_PollEvent(&event)){//Handling the keyboard
-	switch (event.type){
-	    case SDL_QUIT:exit(0);break;
-	    case SDL_KEYDOWN:
-		switch(event.key.keysym.sym){
-		    case SDLK_ESCAPE:exit(0);
-		    case SDLK_w:cubeTran.y+=2;break;
-		    case SDLK_s:cubeTran.y-=2;break;
-		    case SDLK_a:cubeTran.x-=2;break;
-		    case SDLK_d:cubeTran.x+=2;break;
-		    case SDLK_e:scalar+=.1f;break;
-		    case SDLK_q:scalar-=.1f;break;
-		    case SDLK_i:pit+=2;break;
-		    case SDLK_k:pit-=2;break;
-		    case SDLK_j:yaw+=2;break;
-		    case SDLK_l:yaw-=2;break;
-		}
-	}
+  while (SDL_PollEvent(&event)){//Handling the keyboard
+    switch (event.type){
+    case SDL_QUIT:exit(0);break;
+    case SDL_KEYDOWN:
+      switch(event.key.keysym.sym){
+      case SDLK_ESCAPE:exit(0);
+      case SDLK_w:cubeTran.y+=2;break;
+      case SDLK_s:cubeTran.y-=2;break;
+      case SDLK_a:cubeTran.x-=2;break;
+      case SDLK_d:cubeTran.x+=2;break;
+      case SDLK_e:scalar+=.1f;break;
+      case SDLK_q:scalar-=.1f;break;
+      case SDLK_i:pit+=2;break;
+      case SDLK_k:pit-=2;break;
+      case SDLK_j:yaw+=2;break;
+      case SDLK_l:yaw-=2;break;
+      }
+      /*case SDL_MOUSEMOTION:
+        yaw+=((event.motion.x)-300)/10.0;
+        pit+=((event.motion.y)-300)/10.0;
+        SDL_WarpMouseInWindow(screen,300,300);
+				
+        }
+      */
     }
+  }
 }
 
 
 int main(int argc, char **argv){
 	
-	//SDL window and context management
-    SDL_Window *window;
+  //SDL window and context management
+  SDL_Window *window;
 	
-    if(SDL_Init(SDL_INIT_VIDEO)<0){//initilizes the SDL video subsystem
-	fprintf(stderr,"Unable to create window: %s\n", SDL_GetError());
-	SDL_Quit();
-	exit(1);//die on error
-    }
+  if(SDL_Init(SDL_INIT_VIDEO)<0){//initilizes the SDL video subsystem
+    fprintf(stderr,"Unable to create window: %s\n", SDL_GetError());
+    SDL_Quit();
+    exit(1);//die on error
+  }
 
-	//create window
-    window = SDL_CreateWindow(
-	"Lighters", //Window title
-	SDL_WINDOWPOS_UNDEFINED, //initial x position
-	SDL_WINDOWPOS_UNDEFINED, //initial y position
-	500,							//width, in pixels
-	500,							//height, in pixels
-	SDL_WINDOW_OPENGL	//flags to be had
-    );
+  //create window
+  window = SDL_CreateWindow(
+                            "Lighters", //Window title
+                            SDL_WINDOWPOS_UNDEFINED, //initial x position
+                            SDL_WINDOWPOS_UNDEFINED, //initial y position
+                            500,							//width, in pixels
+                            500,							//height, in pixels
+                            SDL_WINDOW_OPENGL	//flags to be had
+                            );
 	
-	//check window creation
-    if(window==NULL){
-	fprintf(stderr,"Unable to create window: %s\n",SDL_GetError());
-    }
+  //check window creation
+  if(window==NULL){
+    fprintf(stderr,"Unable to create window: %s\n",SDL_GetError());
+  }
 	
 
-	//creates opengl context associated with the window
-	SDL_GLContext glcontext=SDL_GL_CreateContext(window);
+  //creates opengl context associated with the window
+  SDL_GLContext glcontext=SDL_GL_CreateContext(window);
 	
-	//initializes glew
+  //initializes glew
   glewExperimental=GL_TRUE;
   if(glewInit()){
     fprintf(stderr, "Unable to initalize GLEW");
     exit(EXIT_FAILURE);
   }
   
-    init();
+  init();
 	
-    while(true){
-	input(window);//keyboard controls
-	display(window);//displaying
-    }
+  while(true){
+    input(window);//keyboard controls
+    display(window);//displaying
+  }
 
-    SDL_GL_DeleteContext(glcontext);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
+  SDL_GL_DeleteContext(glcontext);
+  SDL_DestroyWindow(window);
+  SDL_Quit();
  
-    return 0;
+  return 0;
 }
--- vertexshader.glsl
+++ vertexshader.glsl
@@ -6,10 +6,17 @@ uniform mat4 modelMatrix;
 
 in vec3 in_position;
 
+in vec3 in_normal;
 in vec4 in_color;
+
+
+
 out vec4 pass_color;
+out vec3 normal;
+
 
 void main(){
-  gl_Position=Matrix*viewMatrix*modelMatrix*vec4(in_position,1.0); 
+  gl_Position=Matrix*viewMatrix*modelMatrix*vec4(in_position,1.0);
+  normal = in_normal; 
   pass_color=in_color;
 }
